d389f82256954f69f978619112ee75a37cca72a1
Samuel <salvadomo@hotmail.com>
2023-11-08 13:21:25 -0700

    added questions to js

---
+++ assets/js/script.js
@@ -0,0 +1,28 @@
+//Questions
+const quizData = [
+    {
+        question: "What does the 'localStorage' object in JavaScript do?",
+        choices: ["Stores HTTP requests", "Stores data in the browser", "Stores all inventory", "Stores only health potions"],
+        correctAnswer: "Stores data in the browser",
+    },
+    {
+        question: "What will the brackets in 'function()' do?",
+        choices: ["Multiply", "Nothing", "Call", "Cast void spell"],
+        correctAnswer: "Call",
+    },
+    {
+        question: "How is commenting done for JavaScript?",
+        choices: ["!--comment", "@comment", "//comment", "{comment}"],
+        correctAnswer: "//comment",
+    },
+    {
+        question: "What does HTML stand for?",
+        choices: ["Hyper Text Marketing Lookup", "Hyper Tool Magic Language", "Hyper Text Transfer Protocol", "Hyper Text Markup Language"],
+        correctAnswer: "Hyper Text Markup Language",
+    },
+    {
+        question: "What is CSS used for?",
+        choices: ["Adds structure", "Adds Styling", "Adds point to house Slytherin", "Adds waterfall"],
+        correctAnswer: "Adds Styling",
+    },
+];
